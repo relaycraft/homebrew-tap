fa1c3ae02471eb2d20edddcd007dbfa26167c244
Beta <beta@relaycraft.dev>
2026-02-27 22:54:41 +0800

    initial: add RelayCraft Cask and automation workflow

---
+++ Casks/relaycraft.rb
@@ -0,0 +1,21 @@
+cask "relaycraft" do
+  version "1.0.0-rc.2"
+  sha256 "5ad64988b973a2f8730350e43c02ba5c4a83ee117303128b43832092cb61e625"
+
+  url "https://github.com/relaycraft/relaycraft/releases/download/v#{version}/RelayCraft_#{version}_universal-apple-darwin.dmg"
+  name "RelayCraft"
+  desc "AI-Native Web Traffic Debugging Tool"
+  homepage "https://relaycraft.dev"
+
+  auto_updates true
+
+  app "RelayCraft.app"
+
+  zap trash: [
+    "~/Library/Application Support/relaycraft",
+    "~/Library/Caches/com.beta.relaycraft",
+    "~/Library/Preferences/com.beta.relaycraft.plist",
+    "~/Library/Saved Application State/com.beta.relaycraft.savedState",
+    "~/Library/WebKit/com.beta.relaycraft",
+  ]
+end
